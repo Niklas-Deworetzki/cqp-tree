6cddad8d362c689aa6e7dd936f717b9fb06b8822
Niklas Deworetzki <nikdew@chalmers.se>
2025-12-18 10:57:21 +0100

    Moved common functionality for antlr into its own file.
    # Conflicts:
    #	src/cqp_tree/frontends/grew/translator.py

--- src/cqp_tree/frontends/depsearch/translator.py
+++ src/cqp_tree/frontends/depsearch/translator.py
@@ -1,42 +1,17 @@
-from dataclasses import dataclass, field
-from typing import List, override
-
-from antlr4 import CommonTokenStream, InputStream
-from antlr4.error.ErrorListener import ErrorListener
+from typing import Callable
 
 import cqp_tree.translation as ct
-from cqp_tree.frontends.grew.antlr import GrewLexer, GrewParser
+from cqp_tree.frontends.antlr_utils import make_parse
+from cqp_tree.frontends.depsearch.antlr import DepsearchLexer, DepsearchParser as Depsearch
 
 
-@dataclass
-class ParseErrorListener(ErrorListener):
-    errors: List[ct.InputError] = field(default_factory=list)
-
-    @override
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        self.errors.append(ct.InputError(f'{line}, {column}', msg))
-
-
-def parse(query: str) -> GrewParser.RequestContext:
-    listener = ParseErrorListener()
-
-    lexer = GrewLexer(InputStream(query))
-    stream = CommonTokenStream(lexer)
-    parser = GrewParser(stream)
-
-    for antlr in [lexer, parser]:
-        antlr.removeErrorListeners()
-        antlr.addErrorListener(listener)
-
-    result = parser.request()
-    if listener.errors:
-        raise ct.ParsingFailed(*listener.errors)
-
-    return result
+parse: Callable[[str], Depsearch.QueryContext] = make_parse(
+    DepsearchLexer, Depsearch, Depsearch.query
+)
 
 
 @ct.translator('depsearch')
 def translate_depsearch(depsearch: str) -> ct.QueryPlan:
+    parsed = parse(depsearch)
     ...
-
 
--- src/cqp_tree/frontends/grew/translator.py
+++ src/cqp_tree/frontends/grew/translator.py
@@ -1,39 +1,13 @@
 from collections import defaultdict
-from dataclasses import dataclass, field
-from typing import List, Self, override
+from typing import Self
 
-from antlr4 import CommonTokenStream, InputStream, TerminalNode
-from antlr4.error.ErrorListener import ErrorListener
+from antlr4 import TerminalNode
 
 import cqp_tree.translation as ct
+from cqp_tree.frontends.antlr_utils import make_parse
 from cqp_tree.frontends.grew.antlr import GrewLexer, GrewParser
 
-
-@dataclass
-class ParseErrorListener(ErrorListener):
-    errors: List[ct.InputError] = field(default_factory=list)
-
-    @override
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        self.errors.append(ct.InputError(f'{line}, {column}', msg))
-
-
-def parse(query: str) -> GrewParser.RequestContext:
-    listener = ParseErrorListener()
-
-    lexer = GrewLexer(InputStream(query))
-    stream = CommonTokenStream(lexer)
-    parser = GrewParser(stream)
-
-    for antlr in [lexer, parser]:
-        antlr.removeErrorListeners()
-        antlr.addErrorListener(listener)
-
-    result = parser.request()
-    if listener.errors:
-        raise ct.ParsingFailed(*listener.errors)
-
-    return result
+parse = make_parse(GrewLexer, GrewParser, GrewParser.request)
 
 
 Environment = defaultdict[str, ct.Token]
