87dc6ccf9198f1fd04bf23442767e1a400001cd1
Niklas Deworetzki <nikdew@chalmers.se>
2026-08-28 13:27:07 +0200

    Fixed test code for NoSketch Engine integration tests.

--- tests/integration/noske/test_noske.py
+++ tests/integration/noske/test_noske.py
@@ -10,7 +10,7 @@ from pydantic import BaseModel
 class TestCase(BaseModel):
     description: str
     input: str
-    expected: int
+    expected: list[int] | int
     language: str | None = None
 
 
@@ -26,14 +26,18 @@ class TestNoskeUI(unittest.TestCase):
         self.playwright.stop()
 
     def test_is_up(self):
-        response = self.page.goto(
-            'http://localhost:10070',
-            timeout=10,
-            wait_until='domcontentloaded',
-        )
+        for port, application in (
+            (10070, 'NoSketch Engine'),
+            (31495, 'CQP Tree'),
+        ):
+            response = self.page.goto(
+                f'http://localhost:{port}',
+                timeout=5000,
+                wait_until='domcontentloaded',
+            )
 
-        self.assertIsNotNone(response, 'NoSketch Engine did not start correctly.')
-        self.assertTrue(response.ok, 'NoSketch Engine did not start correctly.')
+            self.assertIsNotNone(response, f'{application} did not start correctly.')
+            self.assertTrue(response.ok, f'{application} did not start correctly.')
 
     def test_query_results(self):
         definition_file = Path(__file__).parent / 'test-cases.yml'
@@ -43,23 +47,40 @@ class TestNoskeUI(unittest.TestCase):
             data = yaml.safe_load(f)
             cases = [TestCase.model_validate(definition) for definition in data]
 
-        for case in cases:
-            with self.subTest(msg=case.description):
+        for case_number, case in enumerate(cases, start=1):
+            print(case)
+            with self.subTest(msg=f'{case_number}: {case.description}'):
                 actual_results = self.do_query(case.input, case.language)
                 expected_results = case.expected
 
-                self.assertEqual(
-                    actual_results,
-                    expected_results,
-                    f'Query expected to produce {expected_results} but got {actual_results} instead.',
-                )
+                if isinstance(expected_results, list):
+                    self.assertIn(
+                        actual_results,
+                        expected_results,
+                        f'Query expected to produce one of {expected_results} but got {actual_results} instead.',
+                    )
+                else:
+                    self.assertEqual(
+                        actual_results,
+                        expected_results,
+                        f'Query expected to produce {expected_results} but got {actual_results} instead.',
+                    )
 
     def do_query(self, query: str, language: Optional[str] = None) -> int:
+        self.page.goto(f'http://localhost:31495', wait_until='domcontentloaded')
         self.page.locator('#query-input-field').fill(query)
         if language:
             self.page.locator('#query-language-selection').select_option(value=language)
         self.page.locator('#submit-query').click()
-        self.page.wait_for_load_state('networkidle')
-        result = self.page.locator('span.size[data-tooltip="Number of hits"]').text_content()
-        result = result.strip().replace(',', '')
-        return int(result)
+
+        with self.page.context.expect_page() as expected_query_result_page:
+            self.page.locator('#run-on-korp-link').click()
+
+        query_result_page = expected_query_result_page.value
+
+        while True:
+            result = query_result_page.locator('span.size[data-tooltip="Number of hits"]').inner_text()
+            result = result.strip()
+            if not result.endswith('.'):
+                result = result.replace(',', '')
+                return int(result)
