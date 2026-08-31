7a3d1ca5b2ccfda507bc07385cf3a5bacc27510a
Niklas Deworetzki <nikdew@chalmers.se>
2026-08-31 18:09:09 +0200

    Try to debug CWB invocation.

--- tests/integration/test_cwb.py
+++ tests/integration/test_cwb.py
@@ -25,9 +25,11 @@ def run_cqp_query(query: str) -> int:
         stdout=subprocess.PIPE,
     )
 
+    print(query)
+
     count = 0
-    for count, _ in enumerate(result.stdout, start=1):
-        pass
+    for line in result.stdout:
+        print(line)
     return count
 
 
